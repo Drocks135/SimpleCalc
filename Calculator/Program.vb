2e970f3f40c3016f5f5ca8bda79ddbf6d2456d34
drocks135 <djclaybaugh135@gmail.com>
2020-04-20 00:22:26 -0400

    Added the use of ans to use last outputted answer

--- Calculator/Program.vb
+++ Calculator/Program.vb
@@ -1,34 +1,60 @@
-Imports System
 Imports System.Text.RegularExpressions
 
 Module Program
     Sub Main(args As String())
         Dim tempNum As String
         Dim exiting As Boolean = False
+        Dim ans As Double = 0
         Console.WriteLine("Welcome to my calculator")
 1:      Do While True
-            Console.WriteLine("Please input a calculation in the form x operation y ie 3 + 2")
+            Console.WriteLine("Please input a calculation in the form x operation y ie 3 + 2, and then press enter")
+            Console.WriteLine("Input ans to use last answer")
             tempNum = Console.ReadLine()
-            If Regex.IsMatch(tempNum, "^[-]?[0-9]+\.*[0-9]*\s*[\*\/\+\-\^]\s*[-]?[0-9]+\.*[0-9]*$") Then
-                Dim mat As Match = Regex.Match(tempNum, "[-]?[0-9]+\.*[0-9]*")
-                Dim numMatch As Match = mat.NextMatch()
+            If Regex.IsMatch(tempNum, "^(\s*[-]?[0-9]+\.*[0-9]*|ans)\s*[\*\/\+\-\^]\s*([-]?[0-9]+\.*[0-9]*|ans)\s*$") Then
+                Dim num1 As Double
+                Dim num2 As Double
+                Dim mat As Match
+                Dim numStr As String
+                Dim op As String
+                If Regex.IsMatch(tempNum, "\s*ans\s*[\+\-\*\/]\s*ans\s*") Then
+                    num1 = ans
+                    num2 = ans
+                    tempNum = Regex.Replace(tempNum, "\s*ans\s*", "")
+                ElseIf Regex.IsMatch(tempNum, "\s*ans\s*[\+\-\/\*]") Then
+                    num1 = ans
+                    mat = Regex.Match(tempNum, "[-]?[0-9]+\.*[0-9]*")
+                    numStr = mat.ToString
+                    num2 = CDbl(numStr)
+                    tempNum = Regex.Replace(tempNum, "\s*ans\s*", "")
 
-                Dim numStr As String = mat.ToString
-                Dim num2Str As String = numMatch.ToString
+                ElseIf Regex.IsMatch(tempNum, "\s*[\+\-\/\*]\s*ans\s*") Then
+                    num2 = ans
+                    mat = Regex.Match(tempNum, "[-]?[0-9]+\.*[0-9]*")
+                    numStr = mat.ToString
+                    num1 = CDbl(numStr)
+                    tempNum = Regex.Replace(tempNum, "\s*ans\s*", "")
 
-                Dim num1 As Double = CDbl(numStr)
-                Dim num2 As Double = CDbl(num2Str)
+                Else
+                    mat = Regex.Match(tempNum, "[-]?[0-9]+\.*[0-9]*")
+                    Dim numMatch As Match = mat.NextMatch()
 
-                Dim op As String = Regex.Replace(tempNum, "\s*[-]?[0-9]+\.*[0-9]*\s*", "")
+                    numStr = mat.ToString
+                    Dim num2Str As String = numMatch.ToString
 
-                Console.WriteLine(op)
+                    num1 = CDbl(numStr)
+                    num2 = CDbl(num2Str)
+                End If
 
-                Dim ans As Double
+                op = Regex.Replace(tempNum, "\s*[-]?[0-9]+\.*[0-9]*\s*", "")
+                If op = "" Then
+                    op = "-"
+                    num2 *= -1
+                End If
                 Select Case op
                     Case "+"
                         ans = num1 + num2
                     Case "-"
-                        ans = num1 + num2
+                        ans = num1 - num2
                     Case "*"
                         ans = num1 * num2
                     Case "/"
